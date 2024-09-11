4ef98d7c22e176a605d80857c8b0d198590efbf5
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-09-11 08:32:20 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import streamlit as st
 if code not in st.session_state:
     st.session_state.code = 0
 
+def load_data():
+    return pd.read_excel("金銭リスト.xlsx")
+ 
+words_df = load_data()
+
 name = st.text_area("名前を入力してください")
 
 if st.button("次へ"):
