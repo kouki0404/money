c757c0cc5d5ae40fd04ebcb7dae96506b7f850b5
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-09-12 09:15:42 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pandas as pd
 import time
+import random
 
+month = [1,2,3,4,5,6,7,8,9,10,11,12]
+if not in onemonth = st.session_state:
+    st.session_state.onemonth = random.randint(0,11)
+game_month = month[st.session_state.onemonth]
 if code not in st.session_state:
     st.session_state.code = 0
 
@@ -15,16 +20,20 @@ words_df = load_data()
 
 name = st.text_area("名前を入力してください")
 
-if st.button("次の日へ"):
+if st.button("次へ"):
+    st.button("次へ") = st.button("性別を決定")
     if name not in " ":
         st.sideber.title("性別を選択してください")
         gender = st.sideber.radio("",("男", "女"), horizontal=True)
         st.write("サイドバーから男女を選んでください(月収が変わります)")
-
-        if gender == "男":
+        
+        if st.button("性別を決定")
             
+            
+            if gender == "男":
+                
 
-        if gender == "女":
+            if gender == "女":
 
     else:
         st.write("名前を入力してください")
