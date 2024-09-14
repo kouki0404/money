9a8d508dcf02c514a0dc36f66148a25746db08b0
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-09-14 04:33:22 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import random
 poetry shell
 python <sky>
 
+man = "あいうえお"
 
 month = [1,2,3,4,5,6,7,8,9,10,11,12]
 if "onemonth" not in st.session_state:
@@ -14,7 +15,7 @@ game_month = month[st.session_state.onemonth]
 if code not in st.session_state:
     st.session_state.code = 0
 
-butonn = "次へ"
+button = "次へ"
 
 @st.cache_date
 def load_data():
@@ -26,18 +27,23 @@ words_df = load_data()
 
 name = st.text_area("名前を入力してください")
 
-if st.button(int(butonn)):
-    butonn = "性別を決定"
+if st.button(int(button)):
+    button = "性別を決定"
     if name not in " ":
         st.sideber.title("性別を選択してください")
         gender = st.sideber.radio("",("男", "女"), horizontal=True)
         st.write("サイドバーから男女を選んでください(月収が変わります)")
         
-        if st.button(int(butonn)):
-            butonn = "次の日へ"
+        if st.button(int(button)):
+            button = "次の日へ"
             
             if gender == "男":
-                st.wirte("a")
+                st.write()
+                st.wirte(man)
+
+                if st.button(int(button))
+                    man = "かきくけこ"
+                    st.write(man)
 
             else:
                 st.write("b")
