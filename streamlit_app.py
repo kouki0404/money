3efbe86cd9bce0aa608718895f388905efa53394
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-09-26 06:40:40 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ if 'days' not in st.session_state:
     st.session_state.days = 1
 if 'code' not in st.session_state:
     st.session_state.code = 0
+totalcount_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+total_days = totalcount_days(st.session_state.month)
 st.sidebar.title("性別を選択してください")
 gender = st.sidebar.radio("",("男", "女"), horizontal=True)
 word = "サイドバーから男女を選んでください(月収が変わります)"
@@ -26,35 +28,20 @@ st.write(str(st.session_state.month) + "月" + str(st.session_state.days) + "日
 if gender == "男":
     st.write("残金 " + str(mens_total) + "円")
     if st.button("次の日へ"):
-        st.session_state.days += 1
-        word = "a" #この部分もexcelで出力
-        st.write(word)
-        words = ["牛肉200g 500円","豚肉300g 450円"]
-        key = st.selectbox("何を買う？",words)
-        if st.session_state.month == 1:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        elif st.session_state.month == 3:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        elif st.session_state.month == 5:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        elif st.session_state.month == 7:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        elif st.session_state.month == 8:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        elif st.session_state.month == 10:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        elif st.session_state.month == 12:
-            if st.session_state.days == 31:
-                st.write("finish!!")
-        if key == "牛肉200g 500円":
-            mens_total -= 500
-            st.session_state.xx += 1
+        if  st.session_state.days <= total_days:
+            st.session_state.started = True
+            st.session_state.finished = False
+            st.session_state.days += 1
+            word = "a" #この部分もexcelで出力
+            st.write(word)
+            words = ["牛肉200g 500円","豚肉300g 450円"]
+            key = st.selectbox("何を買う？",words)
+            if key == "牛肉200g 500円":
+                mens_total -= 500
+                st.session_state.xx += 1
+        
+        else:
+            st.session_state.finished = True
             
 elif gender == "女":
     st.write("残金 " + str(womans_total) + "円")
