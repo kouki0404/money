7b023e203003942bfc772d629d5435c1e049a37a
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-09-26 07:17:25 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,6 @@ st.write(word)
 
 st.write(str(st.session_state.month) + "月" + str(st.session_state.days) + "日")
 if gender == "男":
-    st.write("残金 " + str(mens_total) + "円")
     if st.button("次の日へ"):
         st.session_state.days += 1
         if  st.session_state.days <= total_days:
