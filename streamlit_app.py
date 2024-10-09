fd4d2b663da1364eb594b57566a0f0bf1dba1d67
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-10-09 08:42:03 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,16 @@ mens_money = 13000 + st.session_state.energy #光熱費
 mens_total = 270400 #男の平均月給
 womans_total = 208000 #女の平均月給
 
-if 'month' not in st.session_state:
-    st.session_state.month = random.randint(1,12)
-if 'days' not in st.session_state:
-    st.session_state.days = 1
-if 'code' not in st.session_state:
-    st.session_state.code = 0
+@st.cache_data
+def load_data():
+    main = pd.read_excel("基本ストーリー.xlsx")
+    special = pd.read_excel("金銭リスト.xlsx")
+    return pd.concat([main, special], ignore_index=True)
+
+words_df = load_data()
+
 item_date = ["卵 1パック 300円","米 5kg 2500円","大根 1本200円","豚肉 100g 200円","キャベツ 1玉200円"]
 totalcount_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-total_days = totalcount_days[st.session_state.month]
 st.sidebar.title("性別を選択してください")
 gender = st.sidebar.radio("",("以下から選択してください","男", "女"), horizontal=True)
 if gender == "以下から選択してください":
@@ -27,6 +28,14 @@ if gender == "以下から選択してください":
 st.sidebar.selectbox("基本値段",item_date)
 
 if gender == "男":
+    st.session_state.update({
+        'app_started': True,
+        'finished': False,
+        st.session_state.month = random.randint(1,12),
+        st.session_state.days = 1,
+        st.session_state.code = 0,
+        total_days = totalcount_days[st.session_state.month],
+    })
     st.write(str(st.session_state.month) + "月" + str(st.session_state.days) + "日")
     st.session_state.started = True
     st.session_state.finished = False
