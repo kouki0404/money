f02f8ced25fce5272bf4067666ae20985c776356
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-11-05 09:09:15 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,6 +123,7 @@ else:
 
     if choose == "ゲーム画面":
         st.write(f"初期金額 {st.session_state.current_total} 円 (光熱費が引かれています)")
+        # Chatbot iframe を "ゲーム画面" の選択時に表示
         st.markdown("""
         <iframe
             src="https://www.chatbase.co/chatbot-iframe/nVm1Yf2i4qWPwWDlr9itc"
@@ -131,6 +132,7 @@ else:
             frameborder="0">
         </iframe>
         """, unsafe_allow_html=True)
+
     elif choose in ["肉類", "野菜", "調味料", "その他"]:
         # 画像を2カラムに表示
         col1, col2 = st.columns(2)
