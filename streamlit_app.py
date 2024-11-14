05c5746182776f46e4ba3e332a405053a5a98c8a
kouki0404 <168718919+kouki0404@users.noreply.github.com>
2024-11-14 09:52:37 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -310,11 +310,11 @@ def display_results():
     st.markdown('</div>', unsafe_allow_html=True)
 
 if 'test_started' in st.session_state and not st.session_state.finished:
-    st.subheader(f"問題 {st.session_state.current_question + 1} / {st.session_state.total_questions}")
-    st.subheader(f"{st.session_state.current_question_data['単語']}" if test_type == '英語→日本語' else f"{st.session_state.current_question_data['語の意味']}")
+    st.subheader(f"問題 {st.session_state.current_dish + 1} / {st.session_state.total_dishes}")
+    st.subheader(f"{st.session_state.current_dish_data['単語']}"
     st.markdown('<div class="choices-container">', unsafe_allow_html=True)
     for idx, option in enumerate(st.session_state.options):
-        st.button(option, key=f"button_{st.session_state.current_question}_{idx}", on_click=update_question, args=(option,))
+        st.button(option, key=f"button_{st.session_state.current_dish}_{idx}", on_click=update_dish, args=(option,))
     st.markdown('</div>', unsafe_allow_html=True)
 else:
     if 'test_started' in st.session_state and st.session_state.finished:
